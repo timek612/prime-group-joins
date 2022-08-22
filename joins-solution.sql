28868499bdf98d8729dcc7f8fed3c801a6109c16
timek612 <100965327+timek612@users.noreply.github.com>
2022-08-22 17:03:33 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,2 +1,13 @@
 # prime-group-joins
 
+--1. Get all customers and their addresses.
+SELECT * FROM "customers"
+JOIN "addresses" 
+ON "addresses".customer_id = "customers".id;
+
+--2. Get all orders and their line items (orders, quantity and product).
+SELECT * FROM "orders"
+JOIN "line_items"
+ON "orders".id = "line_items".order_id
+JOIN "products"
+ON "products".id = "line_items".product_id;
