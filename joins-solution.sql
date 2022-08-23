9e21fc14ab3047766a00e203c65bbd9ccfe931cd
timek612 <100965327+timek612@users.noreply.github.com>
2022-08-23 09:43:33 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -31,18 +31,25 @@ WHERE "products".description = 'diet pepsi'
 ;
 
 --5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
-SELECT * FROM "customers" 
+SELECT "customers".first_name, "customers".last_name, COUNT("line_items".order_id) FROM "customers"
 JOIN "addresses"
 ON "customers".id = "addresses".customer_id
 JOIN "orders"
 ON "addresses".id = "orders".address_id
 JOIN "line_items"
 ON "orders".id = "line_items".order_id
+GROUP BY "customers".id
 ;
 
--- as close as I could get
 --6. How many customers do we have?
+SELECT COUNT("customers") FROM "customers";
 
 --7. How many products do we carry?
+SELECT COUNT("products") FROM "products";
 
 --8. What is the total available on-hand quantity of diet pepsi?
+SELECT SUM("warehouse_product".on_hand), "products".description FROM "warehouse_product"
+JOIN "products"
+ON "products".id = "warehouse_product".product_id
+WHERE "warehouse_product".product_id = 6
+GROUP BY "products".description;
